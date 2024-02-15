6abd4c57e7ff9841a3b36716cd33a812c14745d5
unknown <you@example.com>
2024-02-15 07:03:10 +0100

    ceation des classes

---
+++ Brief_2/Poupee.java
@@ -0,0 +1,15 @@
+public abstract class Poupee {
+
+    private int taille;
+    private boolean ouverte;
+
+    es
+
+    public abstract void open();
+
+    public abstract void close();
+
+    public abstract void placerIn(Poupee p);
+
+    public abstract void getOutOf(Poupee p);
+}
---
+++ Brief_2/PoupeeRusse.java
@@ -0,0 +1,19 @@
+ class PoupeeRusse extends Poupee {
+
+
+     public void open() {
+
+     }
+
+     public void close() {
+
+     }
+
+     public void placerIn(Poupee p) {
+
+     }
+
+     public void getOutOf(Poupee p) {
+
+     }
+ }
